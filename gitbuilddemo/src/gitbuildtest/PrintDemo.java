8b845b035c3ec1baf6faaa56a7604108daa72584
ANISH <ANISH@192.168.1.3>
2018-09-22 14:03:40 +0530

    just testing whether it is going to github or not

--- gitbuilddemo/src/gitbuildtest/PrintDemo.java
+++ gitbuilddemo/src/gitbuildtest/PrintDemo.java
@@ -3,7 +3,7 @@ package gitbuildtest;
 public class PrintDemo {
 
 	public static void main(String[] args) {
-	System.out.println("This is a Jenkins build demo 2");
+	System.out.println("This is a Jenkins build demo 3");
 	}
 	}
 	
